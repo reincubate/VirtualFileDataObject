eab465e6de1450b36d251157285377a979360ea5
Dan Hughes <danhughes@rizzen.com>
2015-07-14 12:12:40 +0100

    added signing and nuget package

--- VirtualFileDataObject/Properties/AssemblyInfo.cs
+++ VirtualFileDataObject/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VirtualFileDataObject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Developer-friendly virtual file implementation for .NET!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VirtualFileDataObject")]
